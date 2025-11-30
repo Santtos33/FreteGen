cfd03402bbc0551a37bc2385d580fea6c5cfc64a
Santtos33 <gs895125@gmail.com>
2025-11-29 21:22:48 -0300

    first commit

---
+++ src/main/java/com/FreteGen/FreteGen/FreteGenApplication.java
@@ -0,0 +1,13 @@
+package com.FreteGen.FreteGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FreteGen/FreteGen/FreteGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreteGen.FreteGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
